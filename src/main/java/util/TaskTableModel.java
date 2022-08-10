4bf33a3335ab0304bc9ab282ff5c52609214c5ae
Marllon <marllon_1998@hotmail.com>
2022-08-10 10:32:43 -0300

    Initial Commit

--- src/main/java/util/TaskTableModel.java
+++ src/main/java/util/TaskTableModel.java
@@ -6,7 +6,7 @@
 package util;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;;
+import java.util.ArrayList;
 import java.util.List;
 import javax.swing.table.AbstractTableModel;
 import model.Task;
